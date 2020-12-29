97ab90651d181874f638d8636a131d368b0dbb5b
Ryan Arnason <36246108+RyanArnason@users.noreply.github.com>
2020-12-29 13:48:23 -0800

    Create testStockReader.py
    Added code to test the StockReader class.

---
+++ stock_analysis/testing/testStockReader.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  1 10:24:21 2020
+
+@author: ryanar
+"""
+
+import matplotlib.pyplot as plt
+from stock_analysis import StockReader, StockVisualizer, Technical, AssetGroupVisualizer, StockAnalyzer, AssetGroupAnalyzer, StockModeler 
+from stock_analysis.utils import group_stocks, describe_group, make_portfolio
+
+
+reader = StockReader('2019-01-01','2020-11-21')
+apple = reader.get_ticker_data('AAPL')
+
+#apple.to_csv('apple.csv', index = False, header=True)
+
