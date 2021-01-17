992ce932e728b48a60b7eefe2e07a2f1a836528b
Ryan Arnason <36246108+RyanArnason@users.noreply.github.com>
2021-01-16 19:26:53 -0800

    Create stock_portfolio.py
    Create a asset class and portfolio class.

---
+++ stock_analysis/stock_portfolio.py
@@ -0,0 +1,256 @@
+# -*- coding: utf-8 -*-
+"""
+Classe for technical analysis of assets.
+Created on Sat Oct 31 19:35:28 2020
+
+@author: ryanar
+"""
+
+import math
+import matplotlib.pyplot as plt
+from stock_analysis import StockReader, StockVisualizer, Technical, AssetGroupVisualizer, StockAnalyzer, AssetGroupAnalyzer, StockModeler 
+from stock_analysis.utils import group_stocks, describe_group, make_portfolio
+import numpy as np
+import pandas as pd
+
+
+class Asset:
+    
+    def __init__(self,name, ticker, assetClass='Stock', data=None):
+        
+        """
+        Create a StockAnalyzer by passing in a pandas DataFrame of OHLC data.
+
+        Parameters
+        ----------
+        df : Dataframe
+            Contains the stock open, high, low and close and index is the Date.
+
+        Returns
+        -------
+        None.
+
+        """        
+        self.name = name
+        self.ticker = ticker
+        self.assetClass = assetClass
+        
+        if(data):
+            self.data = data()
+        else:
+            reader = StockReader('2019-01-01','2020-11-21')
+            
+            if(assetClass=='Stock'):
+                self.data = reader.get_ticker_data(self.ticker)
+            else:
+                self.data = reader.get_bitcoin_data()
+                    
+        
+        self.analyzer = StockAnalyzer(self.data)
+        self.visualizer = StockVisualizer(self.data)
+    
+    def getName(self):
+        return self.name 
+    
+    def getTicker(self):
+        return self.ticker  
+    
+    def getAssetClass(self):
+        return self.assetClass  
+    
+    def getData(self):
+        return self.data
+    
+    def getLogReturn(self):
+        
+        logReturn = np.log(self.data / self.data.shift(1))
+        
+        return logReturn.dropna()
+    
+    def getNormalityTests(self):
+        print("Normality Test for {}".format(self.name))
+        print(30 * '-')
+        return self.analyzer.normalityTests()        
+    
+    def getAnnualizedVolatility(self):
+        return self.analyzer.annualized_volatility()
+    
+    def get52WeekHigh(self):
+        return self.analyzer.get52WeekHigh()
+    
+    def get52WeekLow(self):
+        return self.analyzer.get52WeekLow()
+    
+    def getCumulativeReturns(self):
+        return self.analyzer.cumulative_returns()
+    
+    def getCV(self):
+        return self.analyzer.cv()
+    
+    def getQCD(self):
+        return self.analyzer.qcd()
+    
+    def plotTimeFrame(self,column='close'):
+        
+        ax = self.visualizer.evolution_over_time(
+            column, 
+            figsize=(10, 4), 
+            legend=False, 
+            title='%s closing price over time' % (self.name),
+            )
+        
+        
+        self.visualizer.add_reference_line(
+            ax, 
+            x=self.data.high.idxmax(), 
+            color='k', 
+            linestyle=':', 
+            alpha=0.5,
+            label=f'highest value ({self.data.high.idxmax():%b %d})'
+            )
+        
+        ax.set_ylabel('Price ($)')
+        
+    def plotMovingAverage(self,column='close', average=['60D','200D']):
+        
+        ax = self.visualizer.moving_average(column, average, title='%s closing price over time' % (self.name))
+              
+        ax.set_ylabel('Price ($)')    
+
+    def plotRenko(self):
+        self.visualizer.renko()
+        
+    def plotAverageTrueRange(self):
+        self.visualizer.AverageTrueRange()    
+
+    def plotIchimokuCloud(self):
+        self.visualizer.IchimokuCloud()           
+
+    def plotVolume(self):
+        self.visualizer.trade_volume()
+        
+    def plotAfterHours(self):
+        self.visualizer.after_hours_trades()    
+
+    def plotCandleStick(self):
+        self.visualizer.candle_stick()  
+        
+    def plotQQPlot(self):
+        self.visualizer.qqplot()
+        
+    def plotOnBalanceVolume(self):
+        self.visualizer.OnBalanceVolume()
+        
+    def plotBollingerBands(self):
+        self.visualizer.bollingerbands()   
+        
+    def plotMACD(self):
+        self.visualizer.MACD()       
+    
+    def plotHistogram(self, column='close'):
+        self.visualizer.histogram(column)
+        
+    def plotOpenToClose(self):
+        print("plotOpenToClose currently doesn't work.")        
+        self.visualizer.open_to_close()
+        plt.show()
+        
+
+class Portfolio:
+    """ Class for providing analysis of a stock portfolio. """    
+    
+    def __init__(self):
+        """
+        Create a StockAnalyzer by passing in a pandas DataFrame of OHLC data.
+
+        Parameters
+        ----------
+        df : Dataframe
+            Contains the stock open, high, low and close and index is the Date.
+
+        Returns
+        -------
+        None.
+
+        """
+        self.holdings = []
+        
+    def addAsset(self, name, ticker, assetClass='Stock', data=None):
+        self.holdings.append(Asset(name, ticker,assetClass, data))        
+        
+    def getHoldings(self):
+        
+        return self.holdings  
+
+    def listHoldings(self):
+        
+        for holding in self.holdings:            
+            print("Name: %s, Ticker: %s, Asset Type: %s, 52 Week Low: %.2f, 52 Week High: %.2f" % (holding.getName(), holding.getTicker(), holding.getAssetClass(), holding.get52WeekLow(), holding.get52WeekHigh()))
+             
+            
+    def printStatistics(self):
+        for holding in self.holdings:
+            print("Name: %s, CV: %s, Annual Volatility: %.2f, qcd: %.2f" % (holding.getName(), holding.getCV(), holding.getAnnualizedVolatility(), holding.getQCD())) 
+    
+    def getStatistics(self):
+        
+        df = pd.DataFrame()
+        
+        for holding in self.holdings:
+            
+            data = {
+                'Name': holding.getName(),
+                'CV':holding.getCV(),
+                'Annual Volatility':holding.getAnnualizedVolatility(),
+                'qcd':holding.getQCD()
+                }               
+            
+            stock = pd.Series(data)            
+            df = df.append(stock, ignore_index=True)
+            
+        df.set_index('Name', drop=True, inplace=True)    
+            
+        return df    
+            
+    def plotRenkoForHolding(self,ticker):        
+        for holding in self.holdings:
+            if (holding.getTicker() == ticker):
+                holding.plotRenko()        
+
+# Need to implement Portfolio Optimization        
+# https://github.com/yhilpisch/py4fi2nd/blob/master/code/ch13/13_a_statistics.ipynb
+
+
+
+apple = Asset('Apple','AAPL')
+apple.getNormalityTests()
+apple.plotTimeFrame()
+apple.plotAverageTrueRange()
+# apple.plotRenko() 
+# apple.plotQQPlot()
+# apple.plotTimeFrame()
+#apple.plotMovingAverage()
+# apple.plotOpenToClose()  
+#apple.plotOnBalanceVolume() 
+#apple.plotBollingerBands() 
+# apple.plotMACD() 
+# apple.plotIchimokuCloud()    
+
+# bitcoin = Asset('Bitcoin', 'BTC-USD', assetClass='Crypto')
+# bitcoin.plotMACD() 
+# bitcoin.plotIchimokuCloud()
+# bitcoin.plotRenko()     
+
+        
+# myPortfolio = Portfolio()
+# myPortfolio.addAsset('Facebook','FB')
+# myPortfolio.addAsset('Apple' ,'AAPL')
+# myPortfolio.addAsset('Amazon','AMZN')
+# myPortfolio.addAsset('Netflix','NFLX')
+# myPortfolio.addAsset('Google','GOOG')
+
+# myPortfolio.listHoldings()
+# print(myPortfolio.getStatistics())
+# myPortfolio.printStatistics()
+
+# myPortfolio.plotRenkoForHolding('FB')        
