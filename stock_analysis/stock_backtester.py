9186770a0e51e5116f6b235543c9c8e02a4667bd
Ryan Arnason <36246108+RyanArnason@users.noreply.github.com>
2021-01-30 11:20:29 -0800

    Update stock_backtester.py
    Added the mean reversion strategy to the backtester.

--- stock_analysis/stock_backtester.py
+++ stock_analysis/stock_backtester.py
@@ -159,14 +159,40 @@ class BacktestLongOnly(BacktestBase):
                     self.position = 0
         
         self.close_out(bar)
-                    
-             
+        
+    def run_mean_reversion_strategy(self, SMA, threshold):
+        
+        msg = f'\n\nRunning mean reversion strategy | '
+        msg += f'SMA = {SMA} & Threshold = {threshold}'
+        msg += f'\nfixed costs {self.ftc} | '
+        msg += f'proportional costs {self.ptc}'
+        print(msg)
+        print('=' * 55)
+        
+        self.position = 0
+        self.trades = 0
+        self.amount = self.initial_amount
+        
+        self.data['SMA'] = self.data['price'].rolling(SMA).mean()
+        
+        for bar in range(SMA, len(self.data)):
+            if self.position == 0:
+                if(self.data['price'].iloc[bar] < self.data['SMA'].iloc[bar] - threshold):
+                    self.place_buy_order(bar, amount=self.amount)
+                    self.position = 1
+            elif self.position == 1:
+                if(self.data['price'].iloc[bar] < self.data['SMA'].iloc[bar] - threshold):
+                    self.place_sell_order(bar, units=self.units)
+                    self.position = 0
+        self.close_out(bar)            
+                     
                 
 if __name__ == '__main__':
     
     def run_strategies():
         lobt.run_sma_strategy(42,252)
         lobt.run_momentum_strategy(60)
+        lobt.run_mean_reversion_strategy(50,5)
     
 lobt = BacktestLongOnly('AAPL.O','2010-1-1','2019-12-31',10000)
 run_strategies()
