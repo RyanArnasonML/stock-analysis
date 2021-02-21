4143ed6a388f1f4736ebd357cf06b619b74f2817
Ryan Arnason <36246108+RyanArnason@users.noreply.github.com>
2021-02-20 21:38:26 -0800

    Update stock_neuralnetwork.py
    Increased the complexity of the data sequence to predict.

--- stock_analysis/stock_neuralnetwork.py
+++ stock_analysis/stock_neuralnetwork.py
@@ -25,12 +25,22 @@ def set_seeds(seed=100):
     tf.random.set_seed(seed)   
 set_seeds()
 
+def transform(x):
+    y = 0.05 * x ** 2 + 0.2 * x + np.sin(x) + 5
+    y += np.random.standard_normal(len(x)) * 0.2
+    return y
+
 # Create sample data
-a = np.arange(100)
+x = np.linspace(-2 * np.pi, 2 * np.pi, 500)
+a = transform(x)
+
+plt.figure(figsize=(10,6))
+plt.plot(x,a)
+
 # Change a array to a two column vector
 a = a.reshape((len(a), -1))
 
-lags = 3
+lags = 5
 
 # Create batches of lagged sequential data.
 g = TimeseriesGenerator(a, a, length=lags, batch_size=5)
@@ -38,29 +48,30 @@ g = TimeseriesGenerator(a, a, length=lags, batch_size=5)
 # The sequential mode is designed to work with exactly one input tensor and one output tensor.
 model = Sequential()
 
-model.add(SimpleRNN(100, activation='relu', input_shape = (lags, 1)))
+model.add(SimpleRNN(500, activation='relu', input_shape = (lags, 1)))
 
 model.add(Dense(1, activation='linear'))
 
-model.compile(optimizer='adagrad', loss='mse', metrics=['mae'])
+model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
 
 model.summary()
 
-h = model.fit(g, epochs=1000, steps_per_epoch=5, verbose=False)
+model.fit(g, epochs=500, steps_per_epoch=10, verbose=False)
 
-res = pd.DataFrame(h.history)
+x = np.linspace(-6 * np.pi, 6 * np.pi, 1000)
+d = transform(x)
 
-res.tail(3)
+g_ = TimeseriesGenerator(d, d, length=lags, batch_size=len(d))
 
-# Preformance metrics during RNN training
-res.iloc[10:].plot(figsize=(10, 6), style=['--','--']);
+f = list(g_)[0][0].reshape((len(d) - lags, lags, 1))
+y = model.predict(f, verbose=False)
 
-# In sample prediction
-x = np.array([21, 22, 23]).reshape((1, lags, 1))
-y = model.predict(x, verbose=False)
-print(int(round(y[0,0])))
-
-# Out of sample prediction
-x = np.array([1187, 1188, 1189]).reshape((1, lags, 1))
-y = model.predict(x, verbose=False)
-print(int(round(y[0,0])))
+plt.figure(figsize=(10,6))
+plt.plot(x[lags:], d[lags:], label='data', alpha=0.75)
+plt.plot(x[lags:], y, 'r.', label='pred', ms=3)
+plt.axvline(-2*np.pi, c='g' , ls='--')
+plt.axvline(2*np.pi, c='g' ,ls = '--')
+plt.text(-15, 22, 'out-of-sample') 
+plt.text(-2, 22, 'in-sample') 
+plt.text(10, 22, 'out-of-sample') 
+plt.legend()
