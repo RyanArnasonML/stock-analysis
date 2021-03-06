8ba888228f0af2f434f7517e646aa8ea2b918018
Ryan Arnason <36246108+RyanArnason@users.noreply.github.com>
2021-03-06 14:00:18 -0800

    Update momentum.py
    Added the ability to look at the results over multiple time frames with graphs.

--- stock_analysis/momentum.py
+++ stock_analysis/momentum.py
@@ -8,6 +8,7 @@ Created on Sat Mar  6 10:54:08 2021
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 import yfinance as yf
 
 def TSMStrategy(returns, period=1, shorts=False):
@@ -21,8 +22,8 @@ def TSMStrategy(returns, period=1, shorts=False):
     
     return performance
 
-#ticker = 'GME'
-ticker = 'TSLA'
+ticker = 'GME'
+#ticker = 'TSLA'
 
 yfObj = yf.Ticker(ticker)
 data = yfObj.history(start='2000-01-01',end='2020-12-31')
@@ -36,12 +37,6 @@ tot = perf_cum[-1] - 1
 ann = perf_cum[-1] ** (1 / years) -1
 vol = performance.std() * np.sqrt(252)
 rfr = 0.02
-sharpe = (ann - rfr) / vol
-
-print("1-day TSM Strategy yields:" +
-      f"\n\t{tot*100:.2f}% total returns" +
-      f"\n\t{ann*100:.2f}% annual returns" +
-      f"\n\t{sharpe:.2f} Sharpe Ratio")    
 
 gme_ret = np.exp(returns.cumsum())   
 b_tot = gme_ret[-1] - 1
@@ -53,4 +48,54 @@ print("Baseline Buy-and-Hold Strategy yields:" +
       f"\n\t{b_tot*100:.2f}% total returns" +
       f"\n\t{b_ann*100:.2f}% annual returns" +
       f"\n\t{b_sharpe:.2f} Sharpe Ratio")
-     
+
+periods = [3, 5, 10, 15, 30, 90]
+
+fig = plt.figure(figsize=(12, 10))
+gs = fig.add_gridspec(4, 4)
+ax0 = fig.add_subplot(gs[:2, :4])
+ax1 = fig.add_subplot(gs[2:, :2])
+ax2 = fig.add_subplot(gs[2:, 2:])
+ax0.plot((np.exp(returns.cumsum()) - 1) * 100, label=ticker, linestyle='-')
+perf_dict = {'tot_ret': {'buy_and_hold': (np.exp(returns.sum()) - 1)}}
+perf_dict['ann_ret'] = {'buy_and_hold': b_ann}
+perf_dict['sharpe'] = {'buy_and_hold': b_sharpe}
+
+for p in periods:
+    log_perf = TSMStrategy(returns, period=p, shorts=True)
+    perf = np.exp(log_perf.cumsum())
+    perf_dict['tot_ret'][p] = (perf[-1] - 1)
+    ann = (perf[-1] ** (1/years) - 1)
+    perf_dict['ann_ret'][p] = ann
+    vol = log_perf.std() * np.sqrt(252)
+    perf_dict['sharpe'][p] = (ann - rfr) / vol
+    ax0.plot((perf - 1) * 100, label=f'{p}-Day Mean')
+
+ax0.set_ylabel('Returns (%)')
+ax0.set_xlabel('Date')
+ax0.set_title('Cumulative Returns')
+ax0.grid()
+ax0.legend()
+
+_ = [ax1.bar(i, v * 100) for i, v in enumerate(perf_dict['ann_ret'].values())]
+ax1.set_xticks([i for i, k in enumerate(perf_dict['ann_ret'])])
+ax1.set_xticklabels([f'{k}-Day Mean' 
+    if type(k) is int else ticker for 
+    k in perf_dict['ann_ret'].keys()],
+    rotation=45)
+ax1.grid()
+ax1.set_ylabel('Returns (%)')
+ax1.set_xlabel('Strategy')
+ax1.set_title('Annual Returns')
+_ = [ax2.bar(i, v) for i, v in enumerate(perf_dict['sharpe'].values())]
+ax2.set_xticks([i for i, k in enumerate(perf_dict['sharpe'])])
+ax2.set_xticklabels([f'{k}-Day Mean' 
+    if type(k) is int else ticker for 
+    k in perf_dict['sharpe'].keys()],
+    rotation=45)
+ax2.grid()
+ax2.set_ylabel('Sharpe Ratio')
+ax2.set_xlabel('Strategy')
+ax2.set_title('Sharpe Ratio')
+plt.tight_layout()
+plt.show()
