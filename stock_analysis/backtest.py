a68575ea119b0ac43386ffc496d502d65dbdd525
Ryan Arnason <36246108+RyanArnason@users.noreply.github.com>
2021-04-04 20:55:43 -0700

    Update backtest.py
    Added the logic to track the commissions.

--- stock_analysis/backtest.py
+++ stock_analysis/backtest.py
@@ -29,6 +29,8 @@ class TestStrategy(bt.Strategy):
         
         # To keep track of pending orders
         self.order = None
+        self.buyprice = None
+        self.buycomm = None
         
     def notify_order(self, order):
         
@@ -40,9 +42,21 @@ class TestStrategy(bt.Strategy):
         # Attention: broker could reject order if not enough cash
         if order.status in [order.Completed]:
             if order.isbuy():
-                self.log('BUY EXECUTED, %.2f' % order.executed.price)
+                self.log('BUY EXECUTED, Price: %.2f , Cost: %.2f, Comm: %.2f' %
+                         (order.executed.price,
+                          order.executed.value,
+                          order.executed.comm)
+                         )
+                
+                self.buyprice = order.executed.price
+                self.buycomm = order.executed.comm
+                
             elif order.issell():
-                self.log('SELL EXECUTED, %.2f' % order.executed.price)
+                self.log('SELL EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' % 
+                         (order.executed.price,
+                          order.executed.value,
+                          order.executed.comm)
+                         )
                 
             self.bar_executed = len(self)
         
@@ -51,6 +65,15 @@ class TestStrategy(bt.Strategy):
             
         # Write done: no pending order
         self.order = None
+    
+    def notify_trade(self, trade):
+        if not trade.isclosed:
+            return
+        
+        self.log('OPERATION PROFIT, GROSS %.2f, NET %.2f' % 
+                 (trade.pnl,
+                  trade.pnlcomm)
+                 )
     
     def next(self):
         # Simply log the closing price of the series from the reference
@@ -107,6 +130,9 @@ if __name__ == '__main__':
 
     # Set our desired cash start
     cerebro.broker.setcash(100000.0)
+    
+    # Set the commission - 0.1% ... divide by 100 to remove the %
+    cerebro.broker.setcommission(commission=0.001)
     
     # Print out the starting conditions
     print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
